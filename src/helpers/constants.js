4a2a0197219ed7c81133eec61856c0b47b6ed559
Viktoriia Volkova <viktoriafeldman26@gmail.com>
2023-04-08 15:19:52 +0200

    Update consts

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,22 +1,22 @@
 export const RESTORE_PASSWORD_URL =
-  'http://localhost:3000/restore-password?token=';
+  "https://learn-me.netlify.app/restore-password?token=";
 
-export const HEADERS_TOKEN_NAME = 'x-access-token';
-export const REFRESH_TOKEN = 'refreshToken';
-export const ACCESS_TOKEN = 'accessToken';
+export const HEADERS_TOKEN_NAME = "x-access-token";
+export const REFRESH_TOKEN = "refreshToken";
+export const ACCESS_TOKEN = "accessToken";
 
-export const HOST = 'smtp.gmail.com';
-export const EMAIL = 'jsmasterynews@gmail.com';
-export const PASS = 'jicyypvbmkuvzkgl';
-export const USER = 'learnme@gmail.com';
-export const MAIL_SERVICE = 'gmail';
-export const MAIL_SUBJECT = 'LearnMe відновлення паролю';
-export const MAIL_TEXT = 'Відновіть Ваш пароль за посиланням:';
-export const GMAIL_ACCESSTOKEN = 'GMAIL_ACCESSTOKEN';
+export const HOST = "smtp.gmail.com";
+export const EMAIL = "jsmasterynews@gmail.com";
+export const PASS = "jicyypvbmkuvzkgl";
+export const USER = "learnme@gmail.com";
+export const MAIL_SERVICE = "gmail";
+export const MAIL_SUBJECT = "LearnMe відновлення паролю";
+export const MAIL_TEXT = "Відновіть Ваш пароль за посиланням:";
+export const GMAIL_ACCESSTOKEN = "GMAIL_ACCESSTOKEN";
 
-export const CLOUD_NAME = 'dtrtjvdfj';
-export const CLOUDINARY_KEY = '342126722729724';
-export const CLOUDINARY_SECRET = 'GfJWlWyoSORXA49HAQV4AVRheGg';
-export const CLOUDINARY_FOLDER = 'CloudinaryDemo';
+export const CLOUD_NAME = "dtrtjvdfj";
+export const CLOUDINARY_KEY = "342126722729724";
+export const CLOUDINARY_SECRET = "GfJWlWyoSORXA49HAQV4AVRheGg";
+export const CLOUDINARY_FOLDER = "CloudinaryDemo";
 
-export const ORIGIN_PATH = 'http://localhost:3000';
+export const ORIGIN_PATH = "https://learn-me.herokuapp.com/";
